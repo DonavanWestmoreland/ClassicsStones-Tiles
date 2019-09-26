007dab2d0b2f9930bdae8502b19158244a24cf10
Donavan Westmoreland <reytaewn@gmail.com>
2019-09-26 11:11:33 -0500

    Created The order form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-document.getElementById('button').addEventListener("click");
+/**************Order Form *****************/
+var jobArr = ["Basic", "Advanced","Special"];
+var jobPriceArr = [200,400,0]
+jobFill();
+
+function jobFill(){
+    var job = document.getElementById("installType");
+
+    for (i=0;i<jobArr.length;i++){
+        var opt = document.createElement("option");
+        opt.innerHTML = jobArr[i];
+        opt.value = jobArr[i];
+        job.appendChild(opt);
+    }
+}
+
+function describeJobType(){
+    var jobType =document.getElementById("installType").value;
+    
+}
